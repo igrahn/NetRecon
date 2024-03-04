2c83980bf5395a47180c47a949d2ea40e2fff315
igrahn <162189356+igrahn@users.noreply.github.com>
2024-03-04 12:39:31 -0700

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Group member 1: Ian Grahn
+# Group member 2: 
+# CI 201 / 01PC 
+# This script ... 
+
 # Main Menu Function
 main_menu () {
   echo "---------- Main Menu ----------"
