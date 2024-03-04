589d0e79d3e59a0d2161bf4dbdde849a65cc54ec
igrahn <162189356+igrahn@users.noreply.github.com>
2024-03-04 12:36:59 -0700

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-# ....
+# Main Menu Function
+main_menu () {
+  echo "---------- Main Menu ----------"
+}
+# Ping Sweep Function
+
+# Port Scan Function
+
+# Print Scan Results Function
