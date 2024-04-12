4cbf61ddc55281471b8a5cfa05864089900d4014
igrahn <162189356+igrahn@users.noreply.github.com>
2024-04-12 12:11:00 -0700

    setup main menu

--- project2.sh
+++ project2.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-
 # Group member 1: Ian Grahn
 # Group member 2: Calvin Lindemann
 # ERAU CI 201 / 01PC
 # @TODO: add description.
+
+#Main Menu Function
+function mainMenu(){
+choices=("Disk Management" "File Management" "Network Management" "Process Management" "User Account Management" "Utilities")
+
+select option in "${choices[@]}"; do
+  case $option in
+    "Disk Management")
+      diskManagement
+      ;;
+    "File Management")
+      fileManagement
+      ;;
+    "Network Management")
+      fileManagement
+      ;;
+    "Process Management")
+      processManagement
+      ;;
+    "User Account Management")
+      userAccountManagement
+      ;;
+    "Utilities")
+      utilities
+      ;;
+    *)
+      echo -e "Invalid Selection!\n"
+      ;;
+    esac
+  done
+}
