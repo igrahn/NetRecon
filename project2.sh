7256fb87257c50259e98765a5d3e5e85c2bee0c4
igrahn <162189356+igrahn@users.noreply.github.com>
2024-04-13 15:41:47 -0700

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -6,6 +6,7 @@
 
 #Main Menu Function
 function mainMenu() {
+  echo -e "\n---------------Main Menu---------------\n"
 choices=("Disk Management" "File Management" "Network Management" "Process Management" "User Account Management" "Utilities" "Exit")
 
 select option in "${choices[@]}"; do
@@ -39,12 +40,17 @@ select option in "${choices[@]}"; do
 }
 
 function diskManagement() {
-  echo -e "-----Disk Management-----\n"
+  echo -e "\n---------------Disk Management---------------\n"
   choices=("Display device info" "Display disk partition info" "Display block device info" "Display mounted disk info" "Main menu")
   select option in "${choices[@]}"; do
     case $option in
       "Display device info")
-  
+        echo "Device name: " 
+        uname
+        echo -e "\n"
+        echo "Device stats:"
+        uptime
+        echo -e "\n"
       ;;
       "Display disk parititon info")
   
@@ -63,6 +69,7 @@ function diskManagement() {
 }
 
 function fileManagement() {
+  echo -e "\n---------------File Management---------------\n"
   choices=("Present working directory" "List directory contents" "Create a file" "Change file permissions" "Remove a file" "Read a file" "Main menu")
   select option in "${choices[@]}"; do
     case $option in
